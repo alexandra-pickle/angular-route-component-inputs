3051516e98e772d0793935504daba2219086ec85
Alexandra Pickle <alexandra.pickle@gmail.com>
2023-07-06 08:55:23 -0500

    Add child route component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':input',
+    loadComponent: () => import('./app-child.component').then((m) => m.default),
+    data: {
+      input: 'dataParam',
+    },
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
