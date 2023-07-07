d083427d623bf7c4f6bb6e7df00ed211d9e1c790
Alexandra Pickle <alexandra.pickle@gmail.com>
2023-07-07 16:59:08 -0500

    Add page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RaceSeasonsComponent } from './race-seasons';
 
 const routes: Routes = [
   {
-    path: ':input',
-    loadComponent: () => import('./app-child.component').then((m) => m.default),
-    data: {
-      input: 'dataParam',
-    },
+    path: 'races',
+    component: RaceSeasonsComponent,
+    children: [
+      {
+        path: ':input',
+        loadComponent: () =>
+          import('./app-child.component').then((m) => m.default),
+      },
+    ],
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./page-not-found.component').then((m) => m.PageNotFoundComponent),
   },
 ];
 
