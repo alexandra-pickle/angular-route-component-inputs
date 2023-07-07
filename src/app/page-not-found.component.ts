eac0366c61688c51d4e861578e8faf4e870e5368
Alexandra Pickle <alexandra.pickle@gmail.com>
2023-07-07 16:59:08 -0500

    Add page not found

---
+++ src/app/page-not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  template: `
+    <h2>404 Error</h2>
+    <h2>Page Not Found</h2>
+  `,
+})
+export class PageNotFoundComponent {}
